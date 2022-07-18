e2b903d9c2cf5fa0aa436455b550d62758bc2a5f
Karthiga Jayaperumal <99970828+KarthigaJayaperumal@users.noreply.github.com>
2022-07-18 20:39:19 +0530

    Add files via upload

---
+++ Array_July.java
@@ -0,0 +1,25 @@
+
+//Linear Search
+
+public class Array_July {
+public static void main(String[] args) {
+		Array_July aj= new Array_July();
+		aj.sorting();
+	}
+	private  void sorting() {
+		int []numbers= {2,5,8,3,4,6};
+		int key=8;
+		int index=0;
+		for(int i=0;i<numbers.length;i++) {
+		if(numbers[i]==key) {
+			index =i;
+			System.out.println("Number is Present at index "+index);
+		}
+				}}
+}
+
+	
+		
+	
+
+
